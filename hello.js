1ff51d9602cdc668eb34289c68e97f55ba9d5430
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-11-20 22:18:42 +0200

    hw1

--- hello.js
+++ hello.js
@@ -1,1 +1,40 @@
-console.log("hello");
+/ Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//Вивести кожну змінну за допомогою: console.log
+
+
+let a = 'hello'
+console.log(a);
+
+let b = 'owu';
+console.log(b);
+
+let c = 'com'
+console.log('ua');
+
+let d = 1;
+console.log(d);
+
+let e = 10;
+console.log(e);
+
+let f = -999;
+console.log(f);
+
+let g = 123;
+console.log(g);
+
+let h = 3.14;
+console.log(h);
+
+let i = 2.7;
+console.log(i);
+
+let j = 16;
+console.log(j);
+
+let k = true;
+console.log(k);
+
+let l = false;
+console.log(l);
+
