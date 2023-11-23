0923a85ddac4e458a6a4bc57534bbd5f978bdd7e
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-11-23 22:25:32 +0200

    половина дз2

--- 22.11 HW.js
+++ 22.11 HW.js
@@ -3,8 +3,57 @@
 
 let jun = ['java', 'js', 'react', true, 'cat', 'dog'];
 console.log(jun);
+
+
+
 //- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+const book = {
+    title:'POPO',
+    pageCount: 200,
+    genre:'roman'
+};
+
+const book1 = {
+    title:'POPO',
+    pageCount: 200,
+    genre:'roman'
+};
+
+const book2 = {
+    title:'POPO',
+    pageCount: 200,
+    genre:'roman'
+};
 //- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
 // Поле "автори" - являється  масивом. Кожен автор має поля name та age.
+
+const book11 = {
+
+    title:'POPO',
+    pageCount: 20,
+    genre:'roman',
+    authors:'TTTT'
+};
+const book12 = {
+
+    title:'POPO',
+    pageCount: 20,
+    genre:'roman',
+    authors:'TTTT'
+};
+const book13 = {
+
+    title:'POPO',
+    pageCount: 20,
+    genre:'roman',
+    authors:[
+        {name:'petro', age:23}
+
+    ],
+};
+console.log(book13.authors);
+
 //- Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password.
 // Вивести в консоль пароль кожного користувача
+
+
