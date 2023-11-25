633a221a73dfb260d7bc635917e7b3adb4297753
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-11-25 23:04:37 +0200

    ще 1 завд залишилось

--- 22.11 HW.js
+++ 22.11 HW.js
@@ -69,18 +69,110 @@ const users1 = [
     {name: `Kel`, username: `Sva`, password:145},
     {name: `Din`, username: `Chiy`, password:156},
 ];
-console.log(users1.password);
+console.log(users1[0].password);
+console.log(users1[2].password);
+console.log(users1[3].password);
+console.log(users1[7].password);
+console.log(users1[8].password);
+console.log(users1[9].password);
 
 //Логічні розгалуження:
    // - Є змінна х, якій ви надаєте довільне числове значення.
    // Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+
+const x = -3;
+
+if (x!= 0){
+    console.log('Вірно')
+}
+else {
+    console.log('Невірно')
+}
+
 //- Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 //(в першу, другу, третю или четверту частину години).
+
+let time = 101;
+
+if (time >=0 && time <=15){
+    console.log('first part')
+}
+else if (time >=16 && time <=30){
+    console.log('second part')
+}
+else if (time >= 31 && time <=45){
+    console.log('third part' )
+}
+else if (time >= 46 && time <=60){
+    console.log('four part')
+}
+
+else {
+    console.log(false)
+}
+
 //- У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
-//- Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+
+let day  = 23;
+
+if (day >= 0 && day <=15 ){
+    console.log('перша декада')
+}else if(day> 15 && day <=31){
+    console.log('Друга декада')
+}else {
+    alert('WRONG DAY')
+    console.log('ERROR')
+}
+
+//- Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день
+// (можна замість плану на день, назву дня англійською).
+
+//const week = + prompt('enter number of week', 2);
+
+//switch (week) {
+    //case 1:
+    // console.log('hello');
+    // break;
+
+    //case 2:
+    // console.log('haha');
+    // break;
+
+    //case 3:
+    // console.log('hohoho');
+    // break;
+
+    //case 4:
+      //  console.log('hoffghhj');
+     //   break;
+    //case 5:
+      //  console.log('ho34556');
+      //  break;
+   // case 6:
+       // console.log('h2222');
+       // break;
+    //case 7:
+     //   console.log('hohsdfsdgfdg');
+       // break;
+
+   // default:
+     //   console.log('error')
+//}
   //  - Користувач вводить або має два числа.
      //   Потрібно знайти та вивести максимальне число з тих двох .
  //       Також потрібно врахувати коли введені рівні числа.
+
+ let number1 = 78;
+let number2 = +prompt('enter number', 78);
+if (number1=== number2){
+    console.log('number is true')
+}
+else if (number1>number2){
+    console.log('max is: ' +number1)}
+
+else  {
+    console.log('max is:' +number2)
+    }
 
    // - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
     //    за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
