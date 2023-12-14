b59437f9b57438ab82ce5af1faecd1e816d35c0a
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-12-14 22:48:18 +0200

    2 task

--- hw-6.js
+++ hw-6.js
@@ -68,7 +68,16 @@ console.log(numb1);
  //   let nums = [11,21,3];
 //sortNums(nums,'ascending') // [3,11,21]
 //sortNums(nums,'descending') // [21,11,3]
+let nums = [11,21,3];
+const sortNew = nums.sort(function (a,b) {
+ return a-b
+});
+console.log(sortNew);
 
+const sortNew1 = nums.sort(function (a,b){
+ return b-a
+});
+console.log(sortNew1);
 
 //- є масив
 //let coursesAndDurationArray = [
@@ -79,10 +88,29 @@ console.log(numb1);
    // {title: 'FullStack', monthDuration: 7},
     //{title: 'Frontend', monthDuration: 4}
 //];
+
+
 //-- відсортувати його за спаданням за monthDuration
 //-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
 //-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 
+let coursesAndDurationArray = [
+ {title: 'JavaScript Complex', monthDuration: 5},
+ {title: 'Java Complex', monthDuration: 6},
+{title: 'Python Complex', monthDuration: 6},
+{title: 'QA Complex', monthDuration: 4},
+ {title: 'FullStack', monthDuration: 7},
+{title: 'Frontend', monthDuration: 4}
+];
+
+const arrstudy = coursesAndDurationArray.sort(function (a1,a2 ) {
+ return a2.monthDuration - a1.monthDuration
+});
+console.log(arrstudy);
+
+
+const studLeng = coursesAndDurationArray.filter((item)=> item.monthDuration > 5);
+console.log(studLeng);
   //  описати колоду карт (від 6 до туза без джокерів)
 //- знайти піковий туз
 //- всі шістки
