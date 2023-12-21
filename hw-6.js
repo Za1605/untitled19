bf424d3f36788e60d306e5d501531f053c7eeb12
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-12-21 23:06:02 +0200

    зробила все

--- hw-6.js
+++ hw-6.js
@@ -221,5 +221,107 @@ console.log(reduse);
 
 
   //  взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+let courses = [
+ {
+  title: 'JavaScript Complex',
+  monthDuration: 5,
+  hourDuration: 909,
+  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js'],
+  logo: '',
+  price: 0,
+  rating: 5,
+  avgResult: 99
+ },
+ {
+  title: 'Java Complex',
+  monthDuration: 6,
+  hourDuration: 909,
+  modules: ['html',
+   'css',
+   'js',
+   'mysql',
+   'mongodb',
+   'react',
+   'angular',
+   'aws',
+   'docker',
+   'git',
+   'java core',
+   'java advanced'],
+  logo: '',
+  price: 0,
+  rating: 4.998,
+  avgResult: 97
+ },
+ {
+  title: 'Python Complex',
+  monthDuration: 6,
+  hourDuration: 909,
+  modules: ['html',
+   'css',
+   'js',
+   'mysql',
+   'mongodb',
+   'react',
+   'angular',
+   'aws',
+   'docker',
+   'git',
+   'python core',
+   'python advanced'],
+  logo: '',
+  price: 0,
+  rating: 4.812,
+  avgResult: 98
+ },
+ {
+  title: 'QA Complex',
+  monthDuration: 4,
+  hourDuration: 909,
+  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'QA/QC'],
+  logo: '',
+  price: 0,
+  rating: 4.65,
+  avgResult: 97
+ },
+ {
+  title: 'FullStack',
+  monthDuration: 7,
+  hourDuration: 909,
+  modules: ['html',
+   'css',
+   'js',
+   'mysql',
+   'mongodb',
+   'react',
+   'angular',
+   'aws',
+   'docker',
+   'git',
+   'node.js',
+   'python',
+   'java'],
+  logo: '',
+  price: 0,
+  rating: 4.772,
+  avgResult: 100
+ },
+ {
+  title: 'Frontend',
+  monthDuration: 4,
+  hourDuration: 909,
+  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass'],
+  logo: '',
+  price: 0,
+  rating: 4.53,
+  avgResult: 90
+ }
+];
+const popo = courses.filter((value)=> value.modules.includes('sass'));
+console.log(popo);
+
+const popo1 = courses.filter((value)=> value.modules.includes('docker'));
+console.log(popo1);
+
 //--написати пошук всіх об'єктів, в який в modules є sass
 //--написати пошук всіх об'єктів, в який в modules є docker
