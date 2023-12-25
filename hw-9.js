d546e7e87506c7b5cf6dc457e9b0974e318352ec
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-12-25 22:13:02 +0200

    половина

--- hw-9.js
+++ hw-9.js
@@ -1,17 +1,26 @@
 //- створити блок,
-    //- додати йому класи wrap, collapse, alpha, beta
-//- додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
-//- додати цей блок в body.
+
+
 //- клонувати його повністю, та додати клон в body.
-let div = document.createElement(`div`);
+let box = document.createElement(`div`);
+//- додати йому класи wrap, collapse, alpha, beta
+box.classList.add(`wrap`);
+box.classList.add(`collaps`);
+box.classList.add(`alpha`);
+box.classList.add(`beta`);
+//- додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+box.style.backgroundColor= `blue`;
+box.style.fontSize= `13`;
+box.style.color= `red`;
+//- додати цей блок в body.
+document.body.appendChild(block);
 
-div.classList.add(`wrap`);
-div.classList.add(`collaps`);
-div.classList.add(`alpha`);
-div.classList.add(`beta`);
+//- клонувати його повністю, та додати клон в body.
 
-iv.style.backgroundColor= `blue`;
-div.style.fontSize= `13`;
-div.style.color= `red`;
+const blockClone = box.cloneNode(true);
+blockClone.salt.backgroundColor = 'green';
+document.body.appendChild(blockClone);
 
-document.body.appendChild(`div`);
+//- Є масив:
+const arr = ['Main', 'Products', 'About us', 'Contacts']
+// Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
