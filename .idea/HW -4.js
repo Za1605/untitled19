9409c31902545ab0e8b4abaec3dcdfb35c105898
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-02-29 23:27:37 +0200

    did all tasks

--- .idea/HW -4.js
+++ .idea/HW -4.js
@@ -130,3 +130,15 @@ function swap(arr,index1,index2) {
  console.log(swap([11,22,33,44],0,1));
 //- Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 //Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+function exchange(sumUAH, currensyValues, exchangeCurrensy) {
+    for (const currensy of  currensyValues) {
+        if (currensy.currency === exchangeCurrensy) {
+            return sumUAH / currensy.value
+
+        }
+    }
+}
+
+const zzz = exchange(1000, [{currency:'USD',value:40}, {currency:'EUR',value:42}],'USD');
+console.log(zzz);
